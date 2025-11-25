514705b2bbed7293728f11cee5c57f4261fa0f04
caua <cauacouto022@gmail.com>
2025-11-25 17:49:33 -0300

    update: listar usuarios

--- src/main/java/com/mvp/api_turismo/controller/UsuarioController.java
+++ src/main/java/com/mvp/api_turismo/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.mvp.api_turismo.controller;
 
+import com.mvp.api_turismo.dto.AtualizarDto;
 import com.mvp.api_turismo.dto.UsuarioDto;
 import com.mvp.api_turismo.model.Usuario;
 import com.mvp.api_turismo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -24,8 +27,12 @@ public class UsuarioController {
         return ResponseEntity.ok(service.buscarUsuarioPorId(id));
 
     }
+@GetMapping
+    public ResponseEntity<List<Usuario>> buscarUsuario(){
+  return ResponseEntity.ok(service.buscarUsuario());
+    }
     @PutMapping
-    public ResponseEntity<UsuarioDto> atualizarUsuario(@PathVariable(name = "id") Integer id,@RequestBody UsuarioDto dto){
+    public ResponseEntity<AtualizarDto> atualizarUsuario(@PathVariable(name = "id") Integer id, @RequestBody AtualizarDto dto){
         service.atualizarUsuario(id,dto);
         return ResponseEntity.ok().build();
     }
